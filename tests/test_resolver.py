b7ee6cb9d9776d0b81f7905a6985744361e01f4d
Paul Hooijenga <paul.hooijenga@sanoma.com>
2016-09-13 11:01:56 +0200

    add some more override resolver tests

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -6,6 +6,7 @@ def test_simple_overrides():
     resolver.add_override("google.com", "127.0.0.1")
     resolver.add_override("www.google.com", "127.0.0.1")
 
+    assert resolver.resolve_a("google") is None
     assert resolver.resolve_a("google.com") == "127.0.0.1"
     assert resolver.resolve_a("www.google.com") == "127.0.0.1"
     assert resolver.resolve_a("mail.google.com") is None
@@ -33,3 +34,17 @@ def test_double_wildcard_overrides():
     assert resolver.resolve_a("mail.google.com") == "127.0.0.2"
     assert resolver.resolve_a("test.mail.google.com") == "127.0.0.2"
     assert resolver.resolve_a("github.com") is None
+
+
+def test_ipv6():
+    resolver = OverrideResolver()
+    resolver.add_override("google.com", "127.0.0.1")
+
+    assert resolver.resolve_a("google.com", ipv6=False) == "127.0.0.1"
+    assert resolver.resolve_a("google.com", ipv6=True) is None
+
+    resolver = OverrideResolver(ipv6=True)
+    resolver.add_override("google.com", "::1")
+
+    assert resolver.resolve_a("google.com", ipv6=True) == "::1"
+    assert resolver.resolve_a("google.com", ipv6=False) is None
